6d45bacb6ed8c228dcac29a329b1614364381020
Renato Rosa Franco <renato_ny@live.com>
2023-03-08 07:52:44 -0300

    Added base body project layout

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,75 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Icon(
+                Icons.person_outline,
+                size: 120,
+                color:
+                Colors.green,
+              ),
+              const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Peso (Kg)',
+                  labelStyle: TextStyle(
+                    color: Colors.green
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25,
+                ),
+              ),
+              const TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Altura (Cm)',
+                  labelStyle: TextStyle(
+                      color: Colors.green
+                  ),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green
+                  ),
+                  child: const Text('Calcular',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0
+                  ),),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text('Info',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 25.0
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
